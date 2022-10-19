55a592d05e9249bbf0cd86dc25b4300bc050454e
TinkyAlice <armingrossmann11@gmail.com>
2022-10-19 09:23:00 +0200

    alex sollte mal rüber gucken aber auskommentieren

--- client/src/App.js
+++ client/src/App.js
@@ -4,36 +4,36 @@ import './App.css';
 
 function App() {
 
-  const [nameReg, setNameReg] = useState("");
+  const [usernameReg, setUsernameReg] = useState("");
   const [passwordReg, setPasswordReg] = useState("");
 
-  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("")
 
-  const[loginStatus, setLoginStatus] = useState("")
+  const [loginStatus, setLoginStatus] = useState("")
 
   const register = () => {
     Axios.post('http://localhost:3001/register', {
-      name: nameReg,
+      username: usernameReg,
       password: passwordReg
     }).then((response) => {
       console.log(response);
     });
   }
 
-  const login =() => {
-    Axios.post('http://localhost:3001/login',{
-      name: name,
+  const login = () => {
+    Axios.post('http://localhost:3001/login', {
+      username: username,
       password: password,
-    }).then((response) =>{
-/*      if(response.data.message){
+    }).then((response) => {
+      if (response.data.message) {
         setLoginStatus(response.data[0].username)
-      }else{
-        
+      } else {
+
         setLoginStatus(response.data.message)
 
       }
-      */
+
       console.log(response);
     })
   };
@@ -45,7 +45,7 @@ function App() {
         <h1>Registirung</h1>
         <label>Username: </label>
         <input type="text" onChange={(e) => {
-          setNameReg(e.target.value);
+          setUsernameReg(e.target.value);
         }} />
         <label>Password:</label>
         <input type="text" onChange={(e) => {
@@ -59,8 +59,8 @@ function App() {
 
       <div>
         <h1>Login</h1>
-        <input type="text" placeholder='Username' onChange={(e)=>{setName(e.target.value)}}/>
-        <input type="text" placeholder="Password" onChange={(e)=>{setPassword(e.target.value)}}/>
+        <input type="text" placeholder='Username' onChange={(e) => { setUsername(e.target.value) }} />
+        <input type="text" placeholder="Password" onChange={(e) => { setPassword(e.target.value) }} />
         <button onClick={login}> Login</button>
       </div>
 
--- server/index.js
+++ server/index.js
@@ -1,58 +1,99 @@
 const express = require('express')
 const mysql = require('mysql')
 const cors = require('cors');
+const { hash } = require('bcrypt');
+const { response } = require('express');
 
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+/*
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 
-const app = express()
+const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true
+}));
+
+app.use(cookieParser)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+    session({
+        key: "userId",
+        secret: "subscribe",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            expires: 60 * 60 * 24
+        }
+    }),
+)
+*/
 
 const db = mysql.createConnection({
     user: "root",
     host: "localhost",
     password: "root",
-    database: "loginsystem"
-    
+    database: "loginsystem",
+
 });
 
 app.post("/register", (req, res) => {
 
-    const name = req.body.name;
+    const username = req.body.username;
     const password = req.body.password;
 
-    db.query(
-        "INSERT INTO users (name, password) VALUES (?,?)",
-        [name, password],
-        (err, result) => {
+
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
             console.log(err)
-            
-            
-        })
-})
+        }
+        db.query(
+            "INSERT INTO users (username, password) VALUES (?,?)",
+            [username, hash],
+            (err, result) => {
+                console.log(err)
+            }
+        );
+    })
 
-app.post("/login", (req, res) =>{
-    const name = req.body.name;
+
+});
+
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
     const password = req.body.password;
-
+console.log("moin")
     db.query(
-        "SELECT * FROM users WHERE name = ? AND password = ?",
-        [name, password],
-        (err, result) =>{
-            if(err){
-                res.send({err: err})
-            } 
-            
-                if(result.lengt > 0){
-                    res.send(result)
-                }else{
-                    console.log(result)
-                    console.log(name)
-                    console.log(password)
-                    res.send({message : "Wrong username/password combination"});
-                }
+        "SELECT * FROM users WHERE username = ?;",
+        username,
+        (err, result) => {
+            if (err) {
+                res.send({ err: err });
+            }
+
+            if (result.length > 0) {
+                bcrypt.compare(password, result[0].password, (error, response) => {
+                    if (response) {
+                        req.session.user = result;
+                        console.log(req.session.user);
+                        res.send(result);
+                    } else {
+                        res.send({ message: "Wrong username/password combination!" });
+                    }
+                });
+            } else {
+                res.send({ message: "User doesn't exist" });
+            }
         }
     );
-})
+});
 
 app.listen(3001, () => {
     console.log("Bruder es läuft")
